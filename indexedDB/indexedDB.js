d1c188804dd84d91726163b8b6b6f9c41f8951d6
Edy Segura <edysegura@gmail.com>
2018-05-31 20:28:30 -0300

    Creating IndexedDB database

--- indexedDB/indexedDB.js
+++ indexedDB/indexedDB.js
@@ -1,1 +1,19 @@
-console.log('To be implemented')
+'use strict'
+
+let db
+let version = 1
+const request = indexedDB.open('messageStore', version)
+
+request.addEventListener('error', event => console.log(event))
+
+request.addEventListener('upgradeneeded', event => {
+  const upgradeDB = event.target.result
+  upgradeDB.createObjectStore('messages', {
+    keyPath: 'sid'
+  })
+})
+
+request.addEventListener('success', event => {
+  db = event.target.result
+})
+
