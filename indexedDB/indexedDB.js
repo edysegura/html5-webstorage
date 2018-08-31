de28888c24e3c84c10583bc1e1cee8847de23e58
Edy Segura <edysegura@gmail.com>
2018-08-31 18:24:37 -0300

    Revert "Refactoring indexedDB.js to work on IE11"
    This reverts commit 1bc584cb6c81752c5dee7220a38415a746d24816.

--- indexedDB/indexedDB.js
+++ indexedDB/indexedDB.js
@@ -1,65 +1,61 @@
 'use strict'
 
-var pokeData = [
+const pokeData = [
   {
-    id: '004',
-    name: 'Charmander',
-    type: 'Fire'
+    "id": "004",
+    "name": "Charmander",
+    "type": "Fire"
   },
   {
-    id: '005',
-    name: 'Charmeleon',
-    type: 'Fire'
+    "id": "005",
+    "name": "Charmeleon",
+    "type": "Fire"
   },
   {
-    id: '006',
-    name: 'Charizard',
-    type: 'Fire/Flying'
+    "id": "006",
+    "name": "Charizard",
+    "type": "Fire/Flying"
   }
 ]
 
-var version = 2
-var request = indexedDB.open('pokemonStore', version)
-var db
+const version = 2
+const request = indexedDB.open('pokemonStore', version)
+let db
 
-request.addEventListener('upgradeneeded', function(event) {
-  var db = event.target.result
-  var pokemonObjectStore = db.createObjectStore('pokemons', { keyPath: 'id' })
+request.addEventListener('upgradeneeded', event => {
+  const db = event.target.result
+  const pokemonObjectStore = db.createObjectStore('pokemons', { keyPath: 'id' })
   pokemonObjectStore.createIndex('name', 'name', { unique: false })
   pokemonObjectStore.createIndex('type', 'type', { unique: false })
 })
 
-request.addEventListener('error', function(event) {
-  console.log(event)
-})
-
-request.addEventListener('success', function(event) {
+request.addEventListener('error', event => console.log(event))
+request.addEventListener('success', event => {
   db = event.target.result
   saveData()
   loadData()
 })
 
 function saveData() {
-  var transaction = db.transaction(['pokemons'], 'readwrite')
-  var pokemonObjectStore = transaction.objectStore('pokemons')
-  var putInStore = function(pokemon) {
-    pokemonObjectStore.put(pokemon)
-  }
-  pokeData.forEach(putInStore)
+  const transaction = db.transaction(['pokemons'], 'readwrite')
+  const pokemonObjectStore = transaction.objectStore('pokemons')
+  pokeData.forEach(pokemon => pokemonObjectStore.put(pokemon))
 }
 
 function loadData() {
-  var pokemonObjectStore = db
+  const pokemonObjectStore = db
     .transaction(['pokemons'], 'readonly')
     .objectStore('pokemons')
 
-  var listData = function(event) {
-    var cursor = event.target.result
-    if (cursor) {
+  const listData = event => {
+    const cursor = event.target.result
+    if(cursor) {
       console.log(cursor.value)
       cursor.continue()
     }
   }
 
-  pokemonObjectStore.openCursor().addEventListener('success', listData)
+  pokemonObjectStore
+    .openCursor()
+    .addEventListener('success', listData)
 }
